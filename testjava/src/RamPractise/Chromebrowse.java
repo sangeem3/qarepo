52534a22e1bd2f082ebecd1a7eddee40f810c146
sangee <sangee.m3@gmail.com>
2018-03-02 16:56:46 -0600

    no change

--- testjava/src/RamPractise/Chromebrowse.java
+++ testjava/src/RamPractise/Chromebrowse.java
@@ -1,8 +1,21 @@
 package RamPractise;
 
+import java.beans.Statement;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.thoughtworks.selenium.Wait;
 
 public class Chromebrowse {
 
--- testjava/src/RamPractise/iedriver.java
+++ testjava/src/RamPractise/iedriver.java
@@ -8,7 +8,7 @@ public class iedriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-				System.setProperty("webdriver.chrome.driver","C:\\Users\\Ramnew\\Desktop\\Drivers\\IEDriverServer.exe");
+				System.setProperty("webdriver.ie.driver","C:\\Users\\Ramnew\\Desktop\\Drivers\\IEDriverServer.exe");
 				WebDriver driver = new InternetExplorerDriver();
 				driver.get("http://google.com");  
 				System.out.println(driver.getTitle());

1970a12d2fb4b39fc36349f3c6c6e4fd96a7ab02
sangee <sangee.m3@gmail.com>
2018-03-05 18:04:37 -0600

    added print stmt

--- testjava/src/Javatests/reverse.java
+++ testjava/src/Javatests/reverse.java
@@ -5,23 +5,19 @@ public class reverse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s="madam";
-				
-		String M="";
-		
-		for(int i=s.length()-1;i>=0;i--)
-		{
-		
-		M=M+s.charAt(i);
-		
-	}
-	
-		if (s.equals(M))
-		{
-			
+		String s = "madam";
+
+		String M = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+
+			M = M + s.charAt(i);
+
 		}
-		else
-		{
+
+		if (s.equals(M)) {
+			System.out.println("Given string is not a palindrome");
+		} else {
 			System.out.println("Given string is not a palindrome");
 		}
 
